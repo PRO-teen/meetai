af43ecf7bfd9c92b2630405d52162effc6512b52
PRO-teen <ayushyadav5867@gmail.com>
2025-07-29 10:56:52 +0530

    agent data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,14 +3,17 @@ import { createTRPCRouter,  protectedProcedure } from "@/trpc/init";
 import { db } from "@/db";
 import { agentsInsertSchema } from "../schemas";
 import {z} from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns,sql } from "drizzle-orm";
 // import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure.input(z.object({id:z.string
   ()})).query(async({input})=>{
     const [existingAgent]  = await db
-      .select()
+      .select({
+        meetingCount:sql<number>`5`,
+        ...getTableColumns(agents),
+      })
       .from(agents)
       .where(eq(agents.id, input.id))
 
@@ -19,7 +22,10 @@ export const agentsRouter = createTRPCRouter({
   }),
   getMany: protectedProcedure.query(async () => {
     const data = await db
-      .select()
+      .select({
+        meetingCount:sql<number>`5`,
+        ...getTableColumns(agents),
+      })
       .from(agents);
 
     //   await new Promise((resolve)=>setTimeout(resolve,5000));
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,11 +1,12 @@
 "use client";
-
+import {  useSuspenseQuery } from "@tanstack/react-query";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 import { useTRPC } from "@/trpc/client";
-import {  useSuspenseQuery } from "@tanstack/react-query";
-// import { ResponsiveDialog } from "@/components/responsive-dialog";
 
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
+import { EmptyState } from "@/components/empty-state";
 
 
 export const AgentsView = () => {
@@ -14,8 +15,16 @@ export const AgentsView = () => {
 
    
   return (
-    <div>
-        {JSON.stringify(data,null,2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+       <DataTable data={data} columns={columns}/>
+       {data.length===0 &&(
+        <EmptyState
+        title="Create your first agent"
+        description="Create an agent to join your meetings.Each agent will follow your
+        instruction and can interact with participants during the call."
+        />
+       )}
+
     </div>
   )
 };
